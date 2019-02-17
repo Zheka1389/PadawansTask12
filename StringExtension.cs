e3d58edbcdd62b14cde981810f954908caed2327
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-18 01:00:11 +0300

    1

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,7 +6,24 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException();
+            if (source == "")
+                throw new ArgumentException();
+            if (source.Length > 128)
+                return false;
+
+            bool[] char_set = new bool[128];
+            for (int i = 0; i < source.Length; i++)
+            {
+                for (int y = i + 1; y < source.Length; y++)
+                {
+                    if (source[i] == source[y])
+                        return false;
+                }
+
+            }
+            return true;
         }
     }
 }
